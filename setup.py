833e56b5e95b3cf3b1e5bae2aabfea8b199104c1
Joakim Larsen <joakimlarsen88@gmail.com>
2020-10-30 12:12:50 +0100

    pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='energyMapper',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='joaki',
+    author_email='',
+    description=''
+)
